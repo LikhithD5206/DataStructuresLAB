b41926ad8d5b662ec8b56286d36870c97fb2674d
LikhithD5206 <likhithd.cs23@bmsce.ac.in>
2024-11-13 23:05:04 +0530

    Add files via upload

---
+++ fwd/bms.js
@@ -0,0 +1,11 @@
+document.querySelector('.admin').addEventListener('click', () => {
+    window.location.href = 'admin.html';
+});
+
+document.querySelector('.student').addEventListener('click', () => {
+    window.location.href = 'stu.html';
+});
+
+document.querySelector('.faculty').addEventListener('click', () => {
+    window.location.href = 'fac.html';
+});
---
+++ fwd/stu.js
@@ -0,0 +1,6 @@
+document.getElementById("submit-button").addEventListener("click", function() {
+    // Add any form validation here if needed
+
+    // Redirect to tt.html
+    window.location.href = "tt.html";
+});
